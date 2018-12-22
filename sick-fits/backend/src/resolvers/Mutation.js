4002bea3d888ed52892ae069589a285591cb4de0
mughees605 <mughees605@gmail.com>
2018-12-23 04:31:56 +0500

    items creation and primsa yoga flow 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const mutations = {};
+const mutations = {
+ async createItem(parent, args, ctx, info){
+    // TODO if they are loggedIn
+    const item = await ctx.db.mutation.createItem({
+      data:{
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,6 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+const Query = {
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
