533149304448ba9cfb300dbb29584605d296ffe0
mughees605 <mughees605@gmail.com>
2018-12-28 01:38:07 +0500

    DELETE ITEM FRONTEND WORK WITH UPDATE CACHE LOGIC 20

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { ALL_ITEMS_QUERY } from './Items';
+import gql from 'graphql-tag';
+
+const DELETE_ITEM_MUTATION = gql`
+mutation DELETE_ITEM_MUTATION($id: ID!){
+   deleteItem(id: $id){
+     id
+   }
+}
+`
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // first we have to read the data in cache
+    let data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    // filter the items which is deleted
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+    // update the cache with filtered list
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+  }
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => {
+          return (
+            <button onClick={() => {
+              if (confirm("Are you sure you want to delete this item")) {
+                deleteItem();
+              }
+            }}>
+              {this.props.children}
+            </button>
+          )
+        }}
+      </Mutation>
+
+    );
+  }
+}
+
+export default DeleteItem;
