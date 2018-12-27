e292798e0b2d2ca054e042034c4a5863a2dd6ad5
mughees605 <mughees605@gmail.com>
2018-12-28 01:37:40 +0500

    Delete Item backend work 20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,26 +1,35 @@
 const mutations = {
- async createItem(parent, args, ctx, info){
+  async createItem(parent, args, ctx, info) {
     // TODO if they are loggedIn
     const item = await ctx.db.mutation.createItem({
-      data:{
+      data: {
         ...args
       }
     }, info);
 
     return item;
   },
-  updateItem(parent, args, ctx, info){
+  updateItem(parent, args, ctx, info) {
     // first take copy of updates
-    const updates = {...args};
+    const updates = { ...args };
     delete updates.id;
     return ctx.db.mutation.updateItem({
       data: updates,
-      where:{
+      where: {
         id: args.id
       }
     },
-    info
+      info
     )
+  },
+async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{  id  title }`)
+    // permission to deleteitem TODO
+    // Delete It
+    return ctx.db.mutation.deleteItem({ where }, info)
+
   }
 };
 
