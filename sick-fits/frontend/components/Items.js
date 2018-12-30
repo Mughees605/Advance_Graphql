7e259b8fca2cc1ea40fe656616efb4d5970fc578
mughees605 <mughees605@gmail.com>
2018-12-30 17:19:00 +0500

    Items Implemented according to Pagination page query frontend work #23

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,8 +35,13 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page}/>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage, // 0, 4 , 8
+            first: perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -44,7 +50,7 @@ class Items extends Component {
             );
           }}
         </Query>
-        <Pagination page={this.props.page}/>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
