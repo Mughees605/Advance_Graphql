bb6bbac0113440ebb3d7e35024c8379aa21ff5fa
mughees605 <mughees605@gmail.com>
2018-12-30 15:12:32 +0500

    Items Connection Backend QUery #22

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db')
 };
 
 module.exports = Query;
