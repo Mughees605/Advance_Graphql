b91fc1bd7b3bc19232d2dbdbdf32df1626fbab7d
mughees605 <mughees605@gmail.com>
2019-01-03 23:46:19 +0500

    signin resolver with token login #27

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -60,6 +60,27 @@ async deleteItem(parent, args, ctx, info) {
     // Finalllllly we return the user to the browser
     return user;
   },
+  async signin(parent, args, ctx, info){
+    const { email , password } = args;
+    const user = await ctx.db.query.user({  where: { email } });
+    if(!user){
+     throw new Error(`No such user found for email ${email}`)
+    }
+    // check if their password is valid
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid){
+       throw new Error('Invalid Password!');
+    }
+    // generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    })
+    // return user
+    return user;
+  }
 };
 
 module.exports = mutations;
