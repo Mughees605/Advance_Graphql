e21fc3f8817443c25c0f267f259d53712537fbf8
mughees605 <mughees605@gmail.com>
2019-01-05 21:41:46 +0500

    completed reset password backend work #29

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -107,6 +107,43 @@ async deleteItem(parent, args, ctx, info) {
      return { message : "Thanks!"}
      // Email them that reset token
 
+  },
+  async resetPassword(parent, args, ctx, info){
+     // 1. check if the password match
+     if(args.password !== args.confirmPassword){
+        throw new Error("Your passwords don't match!")
+     }
+     //2. check if its a legit reset token
+     //3. check if its expired
+     const [user] = await ctx.db.query.users({
+       where:{
+         resetToken: args.resetToken,
+         resetTokenExpiry_gte: Date.now() - 3600000
+       }
+     })
+     if(!user){
+       throw new Error('This token is either invalid or expired!');
+     }
+     // 4. hash the password
+     const password = await bcrypt.hash(args.password, 10);
+     // 5. save the new password and remove the old resetToken fields
+     const updatedUser = await ctx.db.mutation.updateUser({
+       where: { email: user.email },
+       data:{
+         password,
+         resetToken: null,
+         resetTokenExpiry: null,
+       },
+     });
+     // 6.. Generate the jwt
+     const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+     // 7.. set the jwt cookie
+     ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // 8 return the new user
+    return updatedUser;
   }
 };
 
