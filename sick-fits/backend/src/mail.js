0f158bd80a6cfa2f891e54d5e36d2ab32c23bf1c
mughees605 <mughees605@gmail.com>
2019-01-07 23:58:33 +0500

    makeANiceEmail templated for temporary #31

--- sick-fits/backend/src/mail.js
+++ sick-fits/backend/src/mail.js
@@ -9,4 +9,19 @@ const transport = nodemailer.createTransport({
   }
 });
 
+const makeANiceEmail = text => `
+<div className="email" style="
+  border: 1px solid black;
+  padding: 20px;
+  font-family: sans-serif;
+  line-height: 2;
+  font-size: 20px;
+">
+  <h2>Hello There!</h2>
+  <p>${text}</p>
+  <p>Mughees Siddiqui</p>
+</div>
+`;
+
 exports.transport = transport;
+exports.makeANiceEmail = makeANiceEmail;
