e767a8096864559eb5033ad534dafa37bf7dafe4
mughees605 <mughees605@gmail.com>
2019-01-10 23:54:36 +0500

    users query resolver with checking userId and userPermissions #34

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,6 @@
 const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("./../utils");
+
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
@@ -12,7 +14,18 @@ const Query = {
        id: ctx.request.userId
      }
    }, info)
-  }
+  },
+  async users(parent, args, ctx, info){
+    // 1. check if they are logged in
+    if(!ctx.request.userId){
+      throw new Error("You must be logged in!")
+    }
+    // 2. check if the user have the permission to query all the users
+     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+     // 3. if the user have above permissions ['ADMIN', 'PERMISSIONUPDATE'] then send all users
+     return ctx.db.query.users({}, info)
+  },
 };
 
 module.exports = Query;
