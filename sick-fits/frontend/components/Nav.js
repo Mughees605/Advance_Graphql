4abf4a630772bf213131b9ce56d31cf1fbce884e
mughees605 <mughees605@gmail.com>
2019-01-17 00:34:36 +0500

    TOGGLE CART #39

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout'
@@ -22,6 +24,16 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => {
+                return (
+                  <button onClick={toggleCart}>
+                    My Cart
+                </button>
+                )
+              }}
+            </Mutation>
           </>
         )}
         {!me && (
