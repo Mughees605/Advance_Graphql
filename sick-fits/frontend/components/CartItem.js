cf8136b83ef17bdd73cc28c2eb3d71558bdda9ec
mughees605 <mughees605@gmail.com>
2019-01-19 00:40:28 +0500

    Rendered Remove From CartItem in CartItem component #42

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RemoveFromCart from './RemoveFromCart';
 import formatMoney from '../lib/formatMoney';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
@@ -32,6 +33,7 @@ const CartItem = ({cartItem}) => (
         </em>
       </p>
     </div>
+    <RemoveFromCart id={cartItem.id}/>
   </CartItemStyles>
 )
 
