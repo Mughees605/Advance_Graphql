dd4885e98595ef1e5f97b0dcccf14c9b4ddd28a2
mughees605 <mughees605@gmail.com>
2019-01-18 10:36:10 +0500

    CartItem Component #41

---
+++ sick-fits/frontend/components/CartItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import formatMoney from '../lib/formatMoney';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
+
+const CartItem = ({cartItem}) => (
+  <CartItemStyles>
+
+    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+    <div className="cart-item-details">
+      <h3>{cartItem.item.title}</h3>
+      <p>
+        {formatMoney(cartItem.item.price * cartItem.quantity)}
+        {' - '}
+        <em>
+          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+        </em>
+      </p>
+    </div>
+  </CartItemStyles>
+)
+
+CartItem.propTypes = {
+  cartItem: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    item: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      price: PropTypes.number
+    })
+  })
+}
+
+export default CartItem;
