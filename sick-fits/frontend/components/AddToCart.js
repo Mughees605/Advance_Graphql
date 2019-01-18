e98c4d8bb5b34c790ffe7c953fd9e99b0ef1ee88
mughees605 <mughees605@gmail.com>
2019-01-18 10:37:19 +0500

    Refetching cartItems when cart added #41

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
 mutation addToCart($id: ID!){
@@ -20,6 +21,11 @@ class AddToCart extends React.Component {
         variables={{
           id
         }}
+        refetchQueries = {
+          [{
+            query: CURRENT_USER_QUERY
+          }]
+        }
       >
         {addToCart => <button onClick={addToCart}>Add To Cart</button>}
       </Mutation>
