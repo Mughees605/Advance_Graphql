705058075c29e20e3183c50ff64deccb31d82c7e
mughees605 <mughees605@gmail.com>
2019-01-24 03:52:59 +0500

    Orders Query #53

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -5,48 +5,66 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-  me(parent, args, ctx, info){
-   if(!ctx.request.userId){
-     return null;
-   }
-   return ctx.db.query.user({
-     where:{
-       id: ctx.request.userId
-     }
-   }, info)
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({
+      where: {
+        id: ctx.request.userId
+      }
+    }, info)
   },
-  async users(parent, args, ctx, info){
+  async users(parent, args, ctx, info) {
     // 1. check if they are logged in
-    if(!ctx.request.userId){
+    if (!ctx.request.userId) {
       throw new Error("You must be logged in!")
     }
     // 2. check if the user have the permission to query all the users
-     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
 
-     // 3. if the user have above permissions ['ADMIN', 'PERMISSIONUPDATE'] then send all users
-     return ctx.db.query.users({}, info)
+    // 3. if the user have above permissions ['ADMIN', 'PERMISSIONUPDATE'] then send all users
+    return ctx.db.query.users({}, info)
   },
-  async order(parent, args, ctx, info){
-   //1 Make sure loggedIn
-   if(!ctx.request.userId){
-    throw new Error("You must be logged in!")
-  }
-   //2 Query the current order
-   const order = await ctx.db.query.order({
-     where : {
-       id: args.id
-     }
-   }, info);
-   //3 check permission
-   const ownsOrder = order.user.id === ctx.request.userId;
-   const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+  async order(parent, args, ctx, info) {
+    //1 Make sure loggedIn
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!")
+    }
+    //2 Query the current order
+    const order = await ctx.db.query.order({
+      where: {
+        id: args.id
+      }
+    }, info);
+    //3 check permission
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
 
-   if( !ownsOrder || !hasPermission ){
-    throw new Error('You Cant see this buddy');
-   }
+    if (!ownsOrder || !hasPermission) {
+      throw new Error('You Cant see this buddy');
+    }
 
-   //4 Return the order
-   return order;
+    //4 Return the order
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    console.log(info)
+    //1 Make sure loggedIn
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!")
+    }
+
+    //2 Query  orders
+    const orders = await ctx.db.query.orders({
+      where: {
+        user: {
+          id: ctx.request.userId
+        }
+      }
+    }, info);
+     
+    return orders;
   }
 };
 
